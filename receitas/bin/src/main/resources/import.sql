758cefd955cec25d70e0d79e5a2743a1f9a5fe35
Evandro <evandro.ribeiro@sellbie.com.br>
2022-07-19 18:51:09 -0300

    ajuste

---
+++ receitas/bin/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO dw_receita(nome, intervalos, dias) VALUES('Dipirona', '8hrs', 7);
+INSERT INTO dw_receita(nome, intervalos, dias) VALUES('Neosaldina', '12hrs', 2);
+INSERT INTO dw_receita(nome, intervalos, dias) VALUES('Dorflex', '4hrs', 5);
+INSERT INTO dw_receita(nome, intervalos, dias) VALUES('Resfinol', '4hrs', 10);
+INSERT INTO dw_receita(nome, intervalos, dias) VALUES('Tylenol', '6hrs', 8);
